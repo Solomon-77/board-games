91194227fa73af6b3b7b6efa2789cc3ae3dbb75d
Eustaquio <cyrilleustaquio111222@gmail.com>
2025-05-23 19:02:09 +0800

    UI update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,16 @@
 import { Outlet } from "react-router-dom"
+import Nav from "./Nav"
 
 
 const Layout = () => {
    return (
-      <div>
-         <Outlet/>
+      <div className="bg-white h-screen flex flex-col">
+         <div className="border-b border-neutral-200">
+            <Nav />
+         </div>
+         <div className="flex-1 bg-neutral-100">
+            <Outlet />
+         </div>
       </div>
    )
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,12 @@
+
+
+const Nav = () => {
+   return (
+      <div className="h-[60px] border-neutral-300 flex items-center justify-between max-w-[1250px] mx-auto p-4">
+         <div className="font-semibold text-lg">BoardMaster</div>
+         <div>...</div>
+      </div>
+   )
+}
+
+export default Nav
