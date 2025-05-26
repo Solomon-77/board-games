33c05553f0478efe0249b66007d5cce9b37686a3
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-05-26 20:13:00 +0800

    fix typo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
-import Chess from './games/chess'
+import Chess from './games/Chess'
 
 const App = () => {
    return (
