51bfa3b81f1ccd339bdea6351419edfadff9ab84
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-05-27 16:19:39 +0800

    add chess pieces

--- src/games/Chess.tsx
+++ src/games/Chess.tsx
@@ -1,13 +1,38 @@
 import { useState } from "react"
 
-const createBoard = () => {
-   return Array(8).fill(null).map(() => Array(8).fill(null))
+const pieces = {
+   b_rook: '/chess_piece/black/black_rook.svg',
+   b_knight: '/chess_piece/black/black_knight.svg',
+   b_bishop: '/chess_piece/black/black_bishop.svg',
+   b_queen: '/chess_piece/black/black_queen.svg',
+   b_king: '/chess_piece/black/black_king.svg',
+   b_pawn: '/chess_piece/black/black_pawn.svg',
+   w_rook: '/chess_piece/white/white_rook.svg',
+   w_knight: '/chess_piece/white/white_knight.svg',
+   w_bishop: '/chess_piece/white/white_bishop.svg',
+   w_queen: '/chess_piece/white/white_queen.svg',
+   w_king: '/chess_piece/white/white_king.svg',
+   w_pawn: '/chess_piece/white/white_pawn.svg',
+}
+
+type Piece = keyof typeof pieces;
+type Board = (Piece | null)[][];
+
+const createBoard = (): Board => {
+   const initialBoard: Board = Array(8).fill(null).map(() => Array(8).fill(null));
+
+   initialBoard[0] = ['b_rook', 'b_knight', 'b_bishop', 'b_queen', 'b_king', 'b_bishop', 'b_knight', 'b_rook']
+   initialBoard[1] = ['b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn']
+   initialBoard[6] = ['w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn']
+   initialBoard[7] = ['w_rook', 'w_knight', 'w_bishop', 'w_queen', 'w_king', 'w_bishop', 'w_knight', 'w_rook']
+
+   return initialBoard;
 }
 
 const Chess = () => {
-   const [board] = useState(createBoard)
+   const [board] = useState<Board>(createBoard)
 
-   function getSquareColor(rowIndex: number, colIndex: number) {
+   function getSquareColor(rowIndex: number, colIndex: number): string {
       return (rowIndex + colIndex) % 2 === 0 ? 'bg-white' : 'bg-gray-800'
    }
 
@@ -23,8 +48,15 @@ const Chess = () => {
                row.map((_, colIndex) => (
                   <div
                      key={`${rowIndex} ${colIndex}`}
-                     className={`aspect-square ${getSquareColor(rowIndex, colIndex)}`}
+                     className={`aspect-square grid place-items-center ${getSquareColor(rowIndex, colIndex)}`}
                   >
+                     {board[rowIndex][colIndex] && (
+                        <img
+                           src={pieces[board[rowIndex][colIndex] as Piece]}
+                           alt={board[rowIndex][colIndex]}
+                           className="w-full h-full p-[2px]"
+                        />
+                     )}
                   </div>
                ))
             ))}
