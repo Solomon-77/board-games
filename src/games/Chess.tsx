b305ad116945f281ed92b3d02846f5ae9d811026
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-05-27 20:18:47 +0800

    initialize board refactor

--- src/games/Chess.tsx
+++ src/games/Chess.tsx
@@ -22,8 +22,8 @@ const createBoard = (): Board => {
    const initialBoard: Board = Array(8).fill(null).map(() => Array(8).fill(null));
 
    initialBoard[0] = ['b_rook', 'b_knight', 'b_bishop', 'b_queen', 'b_king', 'b_bishop', 'b_knight', 'b_rook']
-   initialBoard[1] = ['b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn', 'b_pawn']
-   initialBoard[6] = ['w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn', 'w_pawn']
+   initialBoard[1] = Array(8).fill('b_pawn')
+   initialBoard[6] = Array(8).fill('w_pawn')
    initialBoard[7] = ['w_rook', 'w_knight', 'w_bishop', 'w_queen', 'w_king', 'w_bishop', 'w_knight', 'w_rook']
 
    return initialBoard;
