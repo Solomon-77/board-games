cd388c5ce5a62c91ba633ae13a2ad43d39d68437
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-06-07 22:36:44 +0800

    add locate king's position function

--- src/games/Chess.tsx
+++ src/games/Chess.tsx
@@ -260,6 +260,16 @@ const Chess = () => {
       return true
    }
 
+   function findKing(player: Player, board: Board): Position {
+      const king = player === 'white' ? 'w_king' : 'b_king'
+      for (let r = 0; r < 8; r++) {
+         for (let c = 0; c < 8; c++) {
+            if (board[r][c] === king) return { row: r, col: c } // return the king position
+         }
+      }
+      return null
+   }
+
    function isValidMove(piece: Piece, from: Position, to: Position): boolean {
       if (!from || !to) return false;
 
