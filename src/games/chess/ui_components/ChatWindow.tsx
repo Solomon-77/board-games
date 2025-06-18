b4ef8ce664b33b2c093f20246b76317ee00023bd
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-06-18 15:30:24 +0800

    update package and ui chat window

--- src/games/chess/ui_components/ChatWindow.tsx
+++ src/games/chess/ui_components/ChatWindow.tsx
@@ -2,7 +2,11 @@
 
 const ChatWindow = () => {
    return (
-      <div className="flex-1 border">ChatWindow</div>
+      <div className="flex-1 rounded-lg shadow-md bg-white">
+         <div className="p-4">
+            <h1 className="text-center font-semibold text-lg">Chat</h1>
+         </div>
+      </div>
    )
 }
 
